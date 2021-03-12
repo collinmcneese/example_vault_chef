f013f53ae7f9676dce3f3461eae624a1ee1ff591
Collin McNeese <cmcneese@chef.io>
2021-03-12 15:32:48 -0600

    cleans up default recipe, adds additional suites
    Signed-off-by: Collin McNeese <cmcneese@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,19 +4,32 @@
 #
 # Copyright:: 2021, Collin McNeese, All Rights Reserved.
 
-# Fetches the token/secretid to read from Vault via data bag
-#  In a real environment this should be an encrypted data bag or some other secure
-#  location for obtaining this data.
-vault_token = data_bag_item('approle_tokens', 'default')["#{node['example_vault_chef']['vault_approle']}"]['token']
+vault_token = case node['example_vault_chef']['vault_token']
+              when 'data-bag'
+                # Fetches the token/secretid to read from Vault via data bag
+                #  In a real environment this should be an encrypted data bag or some other secure
+                #  location for obtaining this data.
+                data_bag_item('approle_tokens', 'default')["#{node['example_vault_chef']['vault_approle']}"]['token']
+              when 'encrypted-data-bag-from-bag'
+                # Fetches the encrypted data_bag secret key from a data_bag and then uses that encryption key to read the
+                #  Vault secret token from the `encrypted_tokens` data_bag.
+                key_content = data_bag_item('encrypted_data_bag_keys', 'default')['key'].strip()
+                data_bag_item('encrypted_tokens', 'default', key_content)["#{node['example_vault_chef']['vault_approle']}"]['token']
+              when 'encrypted-data-bag-from-file'
+                # Fetches the encrypted data_bag secret key from a local file and then uses that encryption key to read the
+                #  Vault secret token from the `encrypted_tokens` data_bag.
+                cookbook_file '/tmp/keyfile' do
+                  source 'mysecretfile'
+                  owner 'root'
+                  group 'root'
+                  mode '0400'
+                  action :create
+                  compile_time true
+                end
 
-# Fetches the encrypted data_bag secret key from a data_bag and then uses that encryption key to read the
-#  Vault secret token from the `encrypted_tokens` data_bag.
-# key_content = data_bag_item('encrypted_data_bag_keys', 'default')['key'].strip()
-
-# Optionally, store the key in the encrypted_databag_keys data_bag as base64 encoded to obfuscate a bit and then decode it:
-# key_content = Base64.decode64(data_bag_item('encrypted_data_bag_keys', 'default')['base64_key']).strip()
-
-vault_token = data_bag_item('encrypted_tokens', 'default', key_content)["#{node['example_vault_chef']['vault_approle']}"]['token']
+                key_content = Chef::EncryptedDataBagItem.load_secret('/tmp/keyfile')
+                data_bag_item('encrypted_tokens', 'default', key_content)["#{node['example_vault_chef']['vault_approle']}"]['token']
+              end
 
 # Use the get_hashi_vault_object helper from secrets_management to fetch secret data.
 vault_data = get_hashi_vault_object(
