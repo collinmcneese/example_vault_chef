c0969da15df7d692dd59efca3a1bbe1937b5fdd6
Collin McNeese <cmcneese@chef.io>
2021-05-05 10:38:12 -0500

    adds vault namespace support to get_hashi_vault_object helper
    Signed-off-by: Collin McNeese <cmcneese@chef.io>

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,7 @@ default['example_vault_chef']['vault_path']               = 'secret/data/demo'
 default['example_vault_chef']['vault_approle']            = 'chef-role'
 default['example_vault_chef']['vault_token']              = 'data-bag'
 default['example_vault_chef']['api_secret_server']        = 'http://10.0.0.21:10811'
+default['example_vault_chef']['vault_namespace']          = nil
 
 # used with 'token-file' option of 'vault_token' attribute
 default['example_vault_chef']['vault_token_file']         = '/etc/chef/vault_token_file'
